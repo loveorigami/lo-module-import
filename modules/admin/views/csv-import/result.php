90c01dcd34b78c7292514e3a237c282fa35313af
Loveorigami <loveorigami@mail.ru>
2015-11-24 12:52:25 +0300

    fix translate

--- modules/admin/views/csv-import/result.php
+++ modules/admin/views/csv-import/result.php
@@ -4,18 +4,18 @@
  */
 ?>
 
-<h1><?=Yii::t('import/app', 'Import finished')?></h1>
+<h1><?=Yii::t('import', 'Import finished')?></h1>
 
 <?if(!$res):?>
 
-    <div class="alert alert-danger"><?=Yii::t('import/app', 'Import error')?></div>
+    <div class="alert alert-danger"><?=Yii::t('import', 'Import error')?></div>
 
 <?else:?>
 
     <div class="alert alert-success">
 
-        <p><?=Yii::t('import/app', 'Success')?>: <?=$res["ok"]?></p>
-        <p><?=Yii::t('import/app', 'Errors')?>: <?=$res["errors"]?></p>
+        <p><?=Yii::t('import', 'Success')?>: <?=$res["ok"]?></p>
+        <p><?=Yii::t('import', 'Errors')?>: <?=$res["errors"]?></p>
 
     </div>
 
